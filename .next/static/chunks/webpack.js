a17f9444f3c7c1821d8ac9f7ad50a3d596612e02
Vitor Martins Cruz <vitormartinscruz.dev@gmail.com>
2021-02-28 20:21:34 -0300

    Fix dark theme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "139cdfbe1a0f29c0c75f";
+/******/ 	var hotCurrentHash = "c2b60f591fdb95c7d21d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
