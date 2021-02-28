5ad267d01db42e295629eb0597d7b8cdcd59c0c0
Vitor Martins Cruz <vitormartinscruz.dev@gmail.com>
2021-02-28 19:27:46 -0300

    Update README.md

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d590d20baa645a1669e8";
+/******/ 	var hotCurrentHash = "60338d76e32eabe96156";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
